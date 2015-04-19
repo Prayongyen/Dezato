cc845d8bea8135ab0e5f03ce948692074671e455
Prayongyen <bank.prayong@gmail.com>
2015-04-19 17:59:42 +0700

    Add Table All Status

---
+++ app/src/main/java/th/ac/buu/se/s55160077/s55160018/dezato/BiiFragment.java
@@ -0,0 +1,62 @@
+package th.ac.buu.se.s55160077.s55160018.dezato;
+
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by prayong on 19/4/2558.
+ */
+public class BiiFragment extends Fragment {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static BiiFragment newInstance(int sectionNumber) {
+        BiiFragment fragment = new BiiFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public BiiFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_bill, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        SharedPreferences sp = getActivity().getSharedPreferences("TABLE_INFO", Context.MODE_PRIVATE);
+        String txtTableNo = sp.getString("txtTableNo","");
+        ActionBar ab = getActivity().getActionBar();
+        ab.setTitle("Bill Table "+txtTableNo);
+        //ab.setSubtitle("Services");
+        //inflater.inflate(R.menu.menu_back, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+}
