d6da5ad4788ee64a2d58c327b68ad5f6f9a14a1a
Prayongyen <bank.prayong@gmail.com>
2015-04-21 11:50:13 +0700

    Add bill class

---
+++ app/src/main/java/th/ac/buu/se/s55160077/s55160018/dezato/BillItem.java
@@ -0,0 +1,9 @@
+package th.ac.buu.se.s55160077.s55160018.dezato;
+
+/**
+ * Created by prayong on 21/4/2558.
+ */
+public class BillItem {
+    private String table_id;
+    private String order_no;
+}
